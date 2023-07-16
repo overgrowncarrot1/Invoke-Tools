2f4f3386695b61b4c9076e80eb0846f32009c582
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2023-07-15 23:13:27 -0400

    Update Windows-Tools.sh

--- Windows-Tools.sh
+++ Windows-Tools.sh
@@ -19,4 +19,4 @@ wget https://github.com/carlospolop/PEASS-ng/releases/download/20230702-bc7ce3ac
 wget https://github.com/antonioCoco/JuicyPotatoNG/releases/download/v1.1/JuicyPotatoNG.zip
 unzip JuicyPotatoNG.zip
 wget https://github.com/itm4n/PrintSpoofer/releases/download/v1.0/PrintSpoofer64.exe
-
+wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Seatbelt.exe
